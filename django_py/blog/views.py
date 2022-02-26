d75a20422a3962ea15a3ec8236d8cfc1b78533b8
ray <wankaohong@gmail.com>
2022-02-26 00:12:54 -0500

    Feb 25/2022 add profile page.

--- django_py/blog/views.py
+++ django_py/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import post
 # Create your views here.
+
+def home(request):
+    context = {'posts': post.objects.all()}
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html',{'title' : 'About'})
